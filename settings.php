7143411902dece401f42b1f560886f46845f2972
novicaa <novicamiketic@gmail.com>
2015-04-01 14:20:17 +0200

    First commit

---
+++ settings.php
@@ -0,0 +1,8 @@
+<?php
+	$adresa="Poached at Vanderbilt 2355 Vanderbilt Beach Rd.Ste 156"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad="Naples FL 34109";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica=""; // AKo ne znate skracenicu ostavite praznu
+	$telephone=2395930770;// Broj koji se poziva na dugme CALL US
+	$theme="b";//Thema koja se aplicira na sve
+	$transitionefect="fade"; //kucate tranziciju koju zelite na stranama . Vrste tranzicija : "fade", "flip", "pop", "slide", "slidedown", "slideup"
+?>
